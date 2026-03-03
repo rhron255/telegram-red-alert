a603770cbca1a26b5f4fdb4be32237bc446127fb
rhron255 <ronhasleton@gmail.com>
2026-03-04 00:00:26 +0200

    Huge temporal ache refactor

--- src/temporal_cache.py
+++ src/temporal_cache.py
@@ -1,60 +1,47 @@
-import asyncio
-from datetime import datetime
 import logging
-from collections import defaultdict
-from typing import Any, Callable, Coroutine
+from datetime import datetime
 
 from config import CACHE_TIMEOUT
 
 logger = logging.getLogger(__name__)
 
-class _CacheEntry:
-    def __init__(self, value: Any, expiry: float) -> None:
+
+class _CacheEntry[T]:
+    def __init__(self, value: T | None, expiry: float) -> None:
         self.value = value
         self.expiry = expiry
         self.inserted_at = datetime.now()
 
     def is_expired(self) -> bool:
-        return self.expiry + self.inserted_at.timestamp() < datetime.now().timestamp()
+        return self.expiry + self.inserted_at.timestamp() >= datetime.now().timestamp()
 
-class TemporalCache:
+
+class TemporalCache[T]:
     """
     A cache which allows to set an expiry duration for entries in it
     """
 
-    def __init__(self, default_timeout: float = CACHE_TIMEOUT) -> None:
-        self.default_timeout = default_timeout
-        self._cache = defaultdict(lambda: None)
+    def __init__(self) -> None:
+        self._cache = set[_CacheEntry[T]]()
 
-    def _pop(self, key: Any):
-        pass
+    def put(self, value: T, duration: float = CACHE_TIMEOUT) -> None:
+        self._cache.add(_CacheEntry(value, duration))
 
-    def put(self, key: Any, value: Any = None, duration: float = None) -> None:
-        self._cache[key] = _CacheEntry(value, duration or self.default_timeout)
-
-    def get(self, key: Any) -> Any:
-        if key in self._cache:
-            entry = self._cache[key]
-            if entry and not entry.is_expired():
+    def get(self, value: T) -> T | None:
+        for entry in self._cache:
+            if entry.value == value and not entry.is_expired():
                 return entry.value
-            else:
-                self._pop(key)
+            elif entry.value == value:
+                self._cache.remove(entry)
                 return None
         return None
 
-    def __contains__(self, key: Any) -> bool:
+    def __contains__(self, key: T) -> bool:
         return key in self._cache and not self._cache[key].is_expired()
 
-    def __delitem__(self, key: Any) -> None:
-        if key in self._cache:
-            del self._cache[key]
+    def get_all(self) -> list[T]:
+        return [entry.value for entry in self._cache if not entry.is_expired()]
 
-    def __setitem__(self, key: Any, value: Any) -> None:
-        self.put(key, value)
-    
-    def __getitem__(self, key: Any) -> Any:
-        return self.get(key)
-
-    def items(self):
-        return {k: v.value for k, v in self._cache.items() if not v.is_expired()}
-
+    def add_all(self, data: list[T]) -> None:
+        for entry in data:
+            self.put(entry)
